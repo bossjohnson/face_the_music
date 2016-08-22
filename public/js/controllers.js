5e6801a69b2ade7aacc3a6766028d7305d039c0d
Josh Newsom <newsommeister@gmail.com>
2016-08-22 13:43:19 -0600

    API requests made from server

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -1,21 +1,25 @@
 app.controller('testController', testController);
 
 function testController($scope, $http) {
-    var url = 'http://nerdist.com/wp-content/uploads/2015/02/DavidLynch-970x545.jpg';
-    var request = {
-        method: 'POST',
-        url: 'https://api.projectoxford.ai/face/v1.0/detect?returnFaceId=false&returnFaceLandmarks=true&returnFaceAttributes=age,gender,facialHair,glasses',
-        headers: {
-            'Content-Type': 'application/json',
-            'Ocp-Apim-Subscription-Key': // API key goes here
-        },
-        data: {
-            url: url
-        }
-    }
-    $http(request)
+    // var url = 'http://nerdist.com/wp-content/uploads/2015/02/DavidLynch-970x545.jpg';
+    // var request = {
+    //     method: 'POST',
+    //     url: 'https://api.projectoxford.ai/face/v1.0/detect?returnFaceId=false&returnFaceLandmarks=true&returnFaceAttributes=age,gender,facialHair,glasses',
+    //     headers: {
+    //         'Content-Type': 'application/json',
+    //         'Ocp-Apim-Subscription-Key': // API key goes here
+    //     },
+    //     data: {
+    //         url: url
+    //     }
+    // }
+    // $http(request)
+    //     .then(function(data) {
+    //         console.log(data.data);
+    //     });
+    $http.get('/test')
         .then(function(data) {
-            console.log(data.data);
+            console.log(data);
         });
 }
 testController.$inject = ['$scope', '$http'];
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,46 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+require('dotenv').config();
+const https = require('https');
 router.get('/', function(req, res, next) {
-  res.sendStatus(200);
+    res.sendStatus(200);
 });
-router.get('/test', function(req, res, next){
-    
+router.get('/test', function(req, res, next) {
+    console.log('hit route');
+    // var apiBaseUrl = 'https://api.projectoxford.ai/face/v1.0/detect';
+    var hostName = 'api.projectoxford.ai';
+    var queryParams = '?returnFaceId=false&returnFaceLandmarks=true&returnFaceAttributes=age,gender,facialHair,glasses';
+    var faceUrl = 'http://nerdist.com/wp-content/uploads/2015/02/DavidLynch-970x545.jpg';
+
+    var options = {
+        hostname: hostName,
+        path: '/face/v1.0/detect' + queryParams,
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Ocp-Apim-Subscription-Key': process.env.FACE_API_KEY
+        }
+    };
+
+    // console.log(options);
+
+    var request = https.request(options, function(response) {
+        var finished = '';
+        response.on('data', function(data) {
+            finished += data.toString();
+        });
+        response.on('end', function() {
+            res.send(finished);
+        })
+    });
+
+    request.on('error', function(error) {
+        console.log(error);
+    });
+    request.write(JSON.stringify({
+        url: faceUrl
+    }));
+    request.end();
+
 });
 module.exports = router;
