28abfccaf9f9d142aa3a8c14e0f265ecd47e7c7d
Josh Newsom <newsommeister@gmail.com>
2016-08-22 19:27:12 -0600

    Prepare to implement cloudinary image upload

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 var express = require('express');
 var path = require('path');
 var routes = require('./routes/index');
+var bodyParser = require('body-parser');
 var app = express();
 app.use(express.static(path.join(__dirname, 'public')));
+// app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
 app.use('/', routes);
 app.listen(process.env.PORT || 3000);
