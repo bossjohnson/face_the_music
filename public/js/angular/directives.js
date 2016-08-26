f72f2f6d4ab2f9ee24e905a30a17c57e11e982ba
Josh Newsom <newsommeister@gmail.com>
2016-08-26 15:08:32 -0600

    Nose box

--- public/js/angular/directives.js
+++ public/js/angular/directives.js
@@ -16,14 +16,18 @@ app.directive('musicalFace', function() {
                 var faceRect = angular.element('<div></div>');
                 var leftEye = angular.element('<div></div>');
                 var rightEye = angular.element('<div></div>');
+                var mouthBox = angular.element('<div></div>');
+                var noseBox = angular.element('<div></div>');
 
                 element.prepend(faceRect);
                 faceRect.after(leftEye);
                 faceRect.after(rightEye);
+                faceRect.after(mouthBox);
+                faceRect.after(noseBox);
 
                 faceRect.css({
                     'z-index': 1,
-                    border: '3px dotted red',
+                    border: '3px dotted white',
                     position: 'absolute',
                     top: face.faceRectangleTop * scale + 'px',
                     left: face.faceRectangleLeft * scale + 'px',
@@ -36,6 +40,7 @@ app.directive('musicalFace', function() {
                     border: '2px dotted green',
                     'border-radius': '50%',
                     position: 'absolute',
+                    // 'background-color': 'black',
                     top: face.eyeLeftTopY * scale + 'px',
                     left: face.eyeLeftOuterX * scale + 'px',
                     width: (face.eyeLeftInnerX - face.eyeLeftOuterX) * scale + 'px',
@@ -47,11 +52,36 @@ app.directive('musicalFace', function() {
                     border: '2px dotted green',
                     position: 'absolute',
                     'border-radius': '50%',
+                    // 'background-color': 'black',
                     top: face.eyeRightTopY * scale + 'px',
                     left: face.eyeRightInnerX * scale + 'px',
                     width: (face.eyeRightOuterX - face.eyeRightInnerX) * scale + 'px',
                     height: (face.eyeRightBottomY - face.eyeRightTopY) * scale + 'px'
                 });
+
+                mouthBox.css({
+                    'z-index': 1,
+                    border: '3px dotted red',
+                    position: 'absolute',
+                    'border-radius': '25%',
+                    left: face.mouthLeftX * scale + 'px',
+                    width: (face.mouthRightX - face.mouthLeftX) * scale + 'px',
+                    top: face.upperLipTopY * scale + 'px',
+                    height: (face.underLipBottomY - face.upperLipTopY) * scale + 'px'
+                });
+
+                noseBox.css({
+                    'z-index': 1,
+                    border: '3px dotted blue',
+                    position: 'absolute',
+                    'border-radius': '40%',
+                    top: face.noseRootLeftY * scale + 'px',
+                    height: (face.noseTipY - face.noseRootLeftY) * scale + 'px',
+                    left: face.noseLeftAlarOutTipX * scale + 'px',
+                    width: (face.noseRightAlarOutTipX - face.noseLeftAlarOutTipX) * scale + 'px'
+                })
+
+
             }, 1000);
         }
     };
