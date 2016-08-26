c0e53d1d15cebcea6184111c41ecc411a19ded69
Josh Newsom <newsommeister@gmail.com>
2016-08-26 13:47:14 -0600

    Face Rectangles

--- public/js/angular/directives.js
+++ public/js/angular/directives.js
@@ -11,12 +11,16 @@ app.directive('musicalFace', function() {
                 var clientRect = element[0].getBoundingClientRect();
                 var faceRect = angular.element('<div></div>');
                 element.prepend(faceRect);
-                console.log(scope.face.faceRectangleTop);
+                var face = scope.face;
+                var scale = 300 / face.originalHeight;
                 faceRect.css({
-                    height: '1px',
-                    border: '1px dotted black',
-                    position: 'relative',
-                    top: scope.face.faceRectangleTop + 'px'
+                    'z-index': 1,
+                    border: '3px dotted red',
+                    position: 'absolute',
+                    top: face.faceRectangleTop * scale + 'px',
+                    left: face.faceRectangleLeft * scale + 'px',
+                    width: face.faceRectangleWidth * scale + 'px',
+                    height: face.faceRectangleHeight * scale + 'px'
                 });
             }, 1000);
         }
